c000e5dc042a987eb89977b11cbb9646869170dc
Brian Richter <brianorwhatever@w87-168-176.wireless.uvic.ca>
2016-03-12 23:50:56 -0800

    removed map controls

--- app/components/Map.react.js
+++ app/components/Map.react.js
@@ -37,7 +37,7 @@ class Map extends React.Component {
                       maxZoom: 16,
                       minZoom: 12,
                       styles: [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"visibility":"off"}]}],
-                      disableDefaultUI: false
+                      disableDefaultUI: true
                     }}
                     onClick={this.handleMapClick}
                   >
