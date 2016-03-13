ceef426d294885590fb69ff6523b664d4e5470e4
Luigi Poole <luigipoole@outlook.com>
2016-03-13 05:06:02 -0700

    further glory

--- app/components/Sidebar/index.js
+++ app/components/Sidebar/index.js
@@ -13,12 +13,19 @@ export default class Sidebar extends Component {
     			</paper-toolbar>
     			<div>
     				<paper-card className={styles.post}>
-    					<div className={styles.postHeader}>Title here </div>
- 						<div className={styles.postContent}>Leprosy remains a public health issue in poorer parts of the world. In 2007 there were approximately 
- 						255,000 new cases reported worldwide. Leprosy (or Hansen's disease) is a chronic infectious disease. The skin and peripheral nerves of people with leprosy contain leprosy bacteria. 
- 						Leprosy can be cured with a combination of antibiotics. The immune system plays an important role in leprosy and determines if and how the disease will develop.</div>
-  						<div class="card-actions">
-    						<paper-button>Some action</paper-button>
+    					<div className={styles.voter}>
+    						<paper-icon-button className={`${styles.button} ${styles.first}`} icon="arrow-drop-up"/>
+    						<paper-icon-button className={styles.button} icon="arrow-drop-down"/>
+    					</div>
+    					<div className={styles.content}>
+    						<div className={styles.postHeader}>Title here </div>
+ 							<div className={styles.postContent}>Leprosy remains a public health issue in poorer parts of the world. In 2007 there were approximately 
+ 							255,000 new cases reported worldwide. Leprosy (or Hansen's disease) is a chronic infectious disease. The skin and peripheral nerves of people with leprosy contain leprosy bacteria. 
+ 							Leprosy can be cured with a combination of antibiotics. The immune system plays an important role in leprosy and determines if and how the disease will develop.
+ 							</div>
+  							<div className={styles.actions}>
+    							<paper-button raised>Read More</paper-button>
+  							</div>
   						</div>
 					</paper-card>
     			</div>
