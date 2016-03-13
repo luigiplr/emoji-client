61d562b43ede66cff035bd2464c40adb0868e88e
LouisRitchie <louiscritchie@gmail.com>
2016-03-12 16:51:56 -0800

    post format fixed

--- app/components/Reply.react.js
+++ app/components/Reply.react.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
 export default class Reply extends React.Component {
-	
+
 	state = {
-		
+
 	};
-	
+
 	render() {
 		return (
-			<div className="reply">reply</div>
+			<div className="reply">
+				<span>({this.props.postId}) {this.props.author}:  {this.props.text}
+				</span>
+			</div>
 		)
 	}
-	
+
 }
