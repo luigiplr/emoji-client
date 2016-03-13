e706e5c09c812d8359c439b4421f5f7bdbd8566c
Brian Richter <brianorwhatever@w87-168-176.wireless.uvic.ca>
2016-03-12 21:09:32 -0800

    fixed map

--- app/components/Map.react.js
+++ app/components/Map.react.js
@@ -31,18 +31,12 @@ class Map extends React.Component {
                     ref={(map) => console.log(map)}
                     defaultZoom={13}
                     defaultCenter={{lat: 48.4422, lng: -123.3657}}>
-                    {this.props.map.markers.map((marker, index) => {
-                      return (
-                        <Marker
-                          {...marker} />
-                      );
                     defaultOptions={{
                       maxZoom: 16,
                       minZoom: 12,
                       styles: [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"visibility":"off"}]}],
                       disableDefaultUI: true
                     }}
-                    defaultCenter={{lat: 48.4422, lng: -123.3657}}
                     onClick={this.handleMapClick}
                   >
                     {this.props.posts.items.map((post, index) => {
