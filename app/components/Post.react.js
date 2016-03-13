59a5fb86ec192dba8eb752b47243bdc794b41e60
LouisRitchie <louiscritchie@gmail.com>
2016-03-12 16:17:55 -0800

    Reply skeleton updated, no server functionality yet

--- app/components/Post.react.js
+++ app/components/Post.react.js
@@ -2,17 +2,30 @@ import React from 'react'
 import Reply from './Reply.react'
 
 export default class Post extends React.Component {
-	
-	
-	
+
+
+
 	state = {
-		
+
 	};
-	
+
 	render() {
+		var	replyNodes = this.props.data.replies.map(function(reply) {
+			return (
+				<div>
+					{reply.text}
+				</div>
+			);
+
+		});
+
 		return (
-			<div className="post">Post</div>
+			<div className="post">
+				<span><h3>{this.props.data.id} - {this.props.data.author}</h3></span>
+
+				{replyNodes}
+			</div>
 		)
 	}
-	
+
 }
--- app/reducers/posts.js
+++ app/reducers/posts.js
@@ -3,9 +3,16 @@ import * as actions from '../constants/ActionTypes';
 const initialState = {
   isFetching: false,
   didInvalidate: false,
+
   items: [
-    {id: 1, author: "Anonymous", text: "Belmont St seriouly needs some repairs."},
-    {id: 2, author: "Anonymous", text: "My pomeranian is sick, does any know a good vet?"}
+    {id: 1, author: "Anonymous", text: "Belmont St seriouly needs some repairs.",
+      replies:
+      [{text: "I live there too... It's ridiculous"},
+      {text: "just buy a bike."}]},
+    {id: 2, author: "Anonymous", text: "My pomeranian is sick, does any know a good vet?",
+      replies:
+      [{text: "What did you expect buying that breed?"},
+      {text: "We have people living in tents downtown and you're worried about your fancy dog..."}]}
   ]
 }
 
