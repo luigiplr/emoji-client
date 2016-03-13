c6ad17df75194212d3766ec1c4da9694392bd6e5
Brian Richter <brianorwhatever@w87-168-176.wireless.uvic.ca>
2016-03-13 01:34:00 -0800

    emoji picker prop called

--- app/components/EmojiPicker.react.js
+++ app/components/EmojiPicker.react.js
@@ -2,11 +2,14 @@ import React from 'react';
 import emojis from '../utils/emojis'
 
 export default class EmojiPicker extends React.Component {
+  handleChange(e) {
+    e.preventDefault();
+    this.props.changeEmoji(e.target.value);
+  }
 
   render() {
-    console.log(emojis);
     return (
-      <select>
+      <select onChange={this.handleChange.bind(this)}>
         {Object.keys(emojis).map((key) => {
           return <option value={emojis[key]}>{emojis[key]}</option>
         })}
