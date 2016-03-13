feb4a4d2acd8d30e2d18afd07f8ea1edee4e8e87
LouisRitchie <louiscritchie@gmail.com>
2016-03-12 20:54:36 -0800

    Supressed reply/post fields

--- app/components/Post.react.js
+++ app/components/Post.react.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Reply from './Reply.react'
+import Form from './Form.react'
 
 export default class Post extends React.Component {
 
@@ -16,13 +17,13 @@ export default class Post extends React.Component {
 					<Reply postId={reply.id} author={reply.author} text={reply.text}/>
 				</div>
 			);
-
 		});
 
 		return (
 			<div className="post">
 				<h4><span>{this.props.data.id} - {this.props.data.author}: {this.props.data.text}</span></h4>
 				{replyNodes}
+				<Form formType="reply"/>
 				<br />
 			</div>
 		)
