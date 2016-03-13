d5101b6232f80824ec4e7d29b9543d04ebfeade2
Brian Richter <brianorwhatever@w87-168-176.wireless.uvic.ca>
2016-03-12 16:11:38 -0800

    moved header to sidebar

--- app/components/Sidebar.react.js
+++ app/components/Sidebar.react.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Post from './Post.react'
+import Header from './Header.react'
 
 export default class Sidebar extends React.Component {
     render() {
         return (
             <div className="sidebar">
+              <Header />
               {this.props.posts.items.map((post) => {
                 return <Post data={post}/>
               })}
