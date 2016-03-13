7400226bb4b5ad0329871c961fe0c758c7158997
Brian Richter <brianorwhatever@w87-168-176.wireless.uvic.ca>
2016-03-12 23:23:22 -0800

    thread style a bit in

--- app/reducers/posts.js
+++ app/reducers/posts.js
@@ -5,16 +5,15 @@ const initialState = {
   didInvalidate: false,
   composingPost: false,
   items: [
-    {id: 1, author: "Anonymous", text: "Belmont St seriouly needs some repairs.",
+    {id: 1, author: "Anonymous", text: "Belmont St seriouly needs some repairs.",  title: 'Belmont',
       latitude: 48.4422, longitude: -123.3657,
-      category: 'poop',
+      emoji: '🙉',
       replies:
       [{id: 1, author: "jake", text: "I live there too... It's ridiculous"},
       {id: 2, author: "Anonymous", text: "just buy a bike."}]},
-    {id: 2, author: "Anonymous", text: "My pomeranian is sick, does any know a good vet?",
+    {id: 2, author: "Anonymous", text: "My pomeranian is sick, does any know a good vet?", title: 'pomeranian',
       latitude: 48.462344, longitude: -123.312536,
-      category: 'event',
-      emoji: '\u{1F604}',
+      emoji: '💩',
       replies:
       [{id: 1, author: "harrison", text: "What did you expect buying that breed?"},
       {id: 2, author: "someGuy1337", text: "We have people living in tents downtown and you're worried about your fancy dog..."}]}
