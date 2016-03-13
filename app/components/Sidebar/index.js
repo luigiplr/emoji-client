841b14ae2243aacacc44129398eb70b23d3e1a49
Luigi Poole <luigipoole@outlook.com>
2016-03-13 04:43:23 -0700

    more work

--- app/components/Sidebar/index.js
+++ app/components/Sidebar/index.js
@@ -3,14 +3,26 @@ import styles from './style.css'
 
 export default class Sidebar extends Component {
 
-    state = {
-
-    };
+    state = {};
 
     render() {
         return (
-            <div className={styles.sidebar}>
-            </div>
+            <paper-scroll-header-panel className={styles.sidebar}>
+    			<paper-toolbar>
+      				<div>Recent Posts</div>
+    			</paper-toolbar>
+    			<div>
+    				<paper-card className={styles.post}>
+    					<div className={styles.postHeader}>Title here </div>
+ 						<div className={styles.postContent}>Leprosy remains a public health issue in poorer parts of the world. In 2007 there were approximately 
+ 						255,000 new cases reported worldwide. Leprosy (or Hansen's disease) is a chronic infectious disease. The skin and peripheral nerves of people with leprosy contain leprosy bacteria. 
+ 						Leprosy can be cured with a combination of antibiotics. The immune system plays an important role in leprosy and determines if and how the disease will develop.</div>
+  						<div class="card-actions">
+    						<paper-button>Some action</paper-button>
+  						</div>
+					</paper-card>
+    			</div>
+  			</paper-scroll-header-panel>
         )
     }
 }
