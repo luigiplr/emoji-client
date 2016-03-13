259fd9112d3e57445f3b3399ceec99349f7a9275
Luigi Poole <luigipoole@outlook.com>
2016-03-13 07:14:26 -0700

    YEYYYYYYYYYYY

--- app/components/CreatePost/index.js
+++ app/components/CreatePost/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { defer } from 'lodash'
+import { defer, delay } from 'lodash'
+import superagent from 'superagent'
 import styles from './style.css'
 
 class If extends Component {
@@ -11,15 +12,15 @@ class If extends Component {
 export default class CreatePost extends Component {
 
     state = {
-        validated: false,
-        posting: false
+        posting: false,
+        posted: false
     };
 
     handleSubmit = () => {
         const title = this.refs.title.value.trim() || ''
         const username = this.refs.username.value.trim() || 'Anonymoose'
         const text = this.refs.text.value.trim() || ''
-        const emoji = this.refs.emoji.value.trim() || ''
+        const emoji = this.refs.emoji.value.trim().length > 0 ? this.refs.emoji.value.trim() : '😄'
 
         if (title.length === 0) return window.alert('You must set a title!')
 
@@ -30,21 +31,34 @@ export default class CreatePost extends Component {
             emoji
         }
 
-
         this.setState({
             posting: true
         })
 
         defer(() => this.refs.modal.center())
 
+        superagent
+            .post('http://api.vicboard.com/threads')
+            .send(post)
+            .end((err, res) => {
+                console.log(err, res)
+                this.setState({
+                    posting: false,
+                    posted: true
+                })
+                this.props.close()
+            })
+
+
     };
 
     render() {
         return (
             <paper-dialog ref="modal" opened={this.props.open} with-backdrop  entry-animation="scale-up-animation" modal={true} exit-animation="fade-out-animation">
-            <h2 className={styles.title}>{(this.state.posting ? 'Posting..' : 'New Post')}</h2>
-            <If test={!this.state.posting}>
+            
+            <If test={!this.state.posting && !this.props.posted}>
               <div>
+              <h2 className={styles.title}>New Post</h2>
               <paper-input ref="title" focused label="Title" />
               <paper-input ref="username" label="Username" />
               <paper-input ref="emoji" readonly max={1} label="Emoji!">
@@ -55,8 +69,8 @@ export default class CreatePost extends Component {
               <paper-button onClick={this.props.close} className={styles.create}>Close</paper-button>
               </div>
             </If>
-            <If test={this.state.posting}>
-              <paper-spinner className={styles.postingSpinner} alt="Posting" active></paper-spinner>
+            <If test={this.props.posted}>
+              <h2 className={styles.title}>Posted successfully!</h2>
             </If>
           </paper-dialog>
         )
--- app/components/Sidebar/Post.react.js
+++ app/components/Sidebar/Post.react.js
@@ -3,10 +3,21 @@ import styles from './style.css'
 
 export default class Post extends Component {
 
-    state = {};
-
     render() {
         return (
+            <paper-card className={styles.post}>
+    					     <div className={styles.voter}>
+    						      <paper-icon-button className={`${styles.button} ${styles.first}`} icon="arrow-drop-up"/>
+    						      <paper-icon-button className={styles.button} icon="arrow-drop-down"/>
+    					     </div>
+    					     <div className={styles.content}>
+    						    <div className={styles.postHeader}>{this.props.title}</div>
+ 							      <div className={styles.postContent}>{this.props.text}</div>
+  							     <div className={styles.actions}>
+    							     <paper-button raised>Read More</paper-button>
+  							     </div>
+  						    </div>
+			</paper-card>
         )
     }
 }
